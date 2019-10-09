d516e9bb5b59860c23553b404beb494df943e17d
Brandon Avilan <bsavilanr@slabcode.com>
2019-10-08 20:48:57 -0500

    Change port to 4002

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // For routes
 app.use('/messages',message)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4002;
 const server = app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
